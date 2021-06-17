587eb430bbb61a2376bce2a2ace985c5e250ea65
VarunIrani <varunirani0505@gmail.com>
2021-06-17 21:53:40 +0530

    Initial Commit

---
+++ ArraySum.js
@@ -0,0 +1,9 @@
+class ArraySum {
+	constructor () {
+		console.log("Given an array of numbers find it's sum");
+	}
+
+	this.arraySum = (arr) => {
+		// Hi contributer. Please write your code here.
+	}
+}
